e02de3b34bf865f40f86f4ca7208b4109d7ea852
Vincent Semrau <vss5123@psu.edu>
2022-03-02 22:44:08 -0500

    updated stubs, loginUI, and testHarness with some stuff

--- src/model/TestHarness.java
+++ src/model/TestHarness.java
@@ -6,6 +6,9 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.*;
+import Stubs.EmployeeUIStub;
+import Stubs.LoginUIStub;
 import controller.*;
 import view.*;
 import model.*;
@@ -15,102 +18,76 @@ import model.*;
  * @author Group 1 HRM
  */
 public class TestHarness {
-    
-    public TestHarness() {
-        testLoginCntl();
+     
+    private static Employee retriveEmployeeFromDatabase(String courseid){
+        Employee employee = new Employee();
+        employee.setFirstName("Vincent");
+        employee.setLastName("Semrau");
+        employee.setDepartment("Human resources");
+        employee.setEmployeeID(180880);
+        employee.setNumLeaveDays(14);
+        employee.setFullTime(true);
+        employee.setUserName("vss5123");
+        employee.setPw("12345");
+        return employee;
+
     }
+
+    private static Complaint retrieveComplaintFromDatabase(Employee employee){
+        Complaint complaint = new Complaint(new Employee(), new Date(), new Employee(), false);
+        complaint.setAssignedEmployee(new Employee());
+        complaint.setOpenDate(new Date());
+        complaint.setClaimant(new Employee());
+        complaint.setOpen(false);
+        return complaint; 
+    }
+
+    private static TimeSubmission retrieveTimeSubmissionFromDatabase(Employee employee){
+        TimeSubmission timeSubmission = new TimeSubmission(0, new Date(), new Date(), new Employee());
+        timeSubmission.setEmployee(new Employee());
+        timeSubmission.setStartTimeFrame(new Date());
+        timeSubmission.setEndTimeFrame(new Date());
+        timeSubmission.setTotalHours(0);
+        return timeSubmission;
+       
+    }
+    private static VacationRequest retreiveVacationRequestFromDatabase(String login){
+        VacationRequest vacationRequest = new VacationRequest(false, 4, new Employee(), new VacationEligibility(), new Employee());
+        vacationRequest.setApproved(false);
+        vacationRequest.setAssignedEmployee(new Employee());
+        vacationRequest.setNumDaysRequested(5);
+        vacationRequest.setRequestingEmployee(new Employee());
+        vacationRequest.setVacationEleigibility(new VacationEligibility());
+        return vacationRequest;
+    }
+
     
+
     //Main method
     public static void main(String[] args) {
-        // public Employee(String firstName, String lastName, String userName, String pw, String department, int employeeID, int numLeaveDays, PreviousTimesheets previousTimesheets, Boolean fullTime) {
+
+        EmployeeUI eView = new EmployeeUIStub();
+        LoginUI lView = new LoginUIStub(); 
+        Employee employee1 = new Employee("Vincent", "Semrau", "vss5123", "12345", "Human Resources", 180880, 14, true);
+        Employee employee2 = new Employee("Frankie", "Ansell", "kla349", "12345", "Human Resources", 179384, 5, false);
+
+
+
+        // loginView lview = new loginViewStub();
+        // Faculty fac = retriveFacultyFromDatabase(lview.getLogin());
+        // if(fac.authenticate(lview.getPwd())){
+        //     Course model  = retriveCourseFromDatabase(fac.getCourseID());
+        //     CourseView view = new CourseViewStub();
+        //     CourseControllerStub controller = new CourseControllerStub(fac, model, view);
+        //     controller.updateView();
+        //     }
+        // else
+        //     System.out.println("Invalid Login/Password");
+        }
 
         
-        // Employee employee1 = new Employee("Vincent", "Semrau", "vss5123", "12345", "Human Resources", 180880, 14, null, true);
-        // Employee employee2 = new Employee("Frankie", "Ansell", "kla349", "12345", "Human Resources", 179384, 5, null, false);
-
         
 
     }
-    public void testLoginCntl(){
-    
-        
-//        Address add1 = new Address(206, "Hub Floor 2", "University Park", "PA", "16802");
-//        Address add2 = new Address(314, "Hub Floor 3", "University Park", "PA", "16802");
-//        Address add3 = new Address(111, "Hub Floor 1", "University Park", "PA", "16802");
-//        Address add4 = new Address(242, "Hub Floor 2", "University Park", "PA", "16802");
-//        Joinable org1 = new Service("THON org", "THON special interest org", "thon@psu.edu", "Raise money FTK", "@THON", "service", add1);
-//        Joinable org2 = new Fitness("Club Sport", "expert", "Hockey", "clubhockey@psu.edu", "Play hockey against other club teams", "@PSUclubhockey", "fitness", add2);
-//        Joinable org3 = new Service("Sustainability org", "Days for girls", "daysforgirls@psu.edu", "Sew reusable kits", "@DFGPennState", "service", add3);
-//        Joinable org4 = new Fitness("Dance Team", "beginner", "Ballroom dance", "ballroom@psu.edu", "Learn how to ballroom dance", "@PSUballroom", "fitness", add4);
-//        organizations.add(org1);
-//        organizations.add(org2);
-//        organizations.add(org3);
-//        organizations.add(org4);
-//        for (Joinable organization : organizations) {
-//            organization.joinOrg();
-//            organization.print(); 
-        }
-    }   
-    
-//    public void testClassHierarchy(){
-//        ArrayList<Organization> organizations = new ArrayList<>();
-//        Address add1 = new Address(206, "Hub Floor 2", "University Park", "PA", "16802");
-//        Address add2 = new Address(314, "Hub Floor 3", "University Park", "PA", "16802");
-//        Address add3 = new Address(111, "Hub Floor 1", "University Park", "PA", "16802");
-//        Address add4 = new Address(242, "Hub Floor 2", "University Park", "PA", "16802");
-//        Organization org1 = new Service("THON org", "THON special interest org", "thon@psu.edu", "Raise money FTK", "@THON", "service", add1);
-//        Organization org2 = new Fitness("Club Sport", "expert", "Hockey", "clubhockey@psu.edu", "Play hockey against other club teams", "@PSUclubhockey", "fitness", add2);
-//        Organization org3 = new Service("Sustainability org", "Days for girls", "daysforgirls@psu.edu", "Sew reusable kits", "@DFGPennState", "service", add3);
-//        Organization org4 = new Fitness("Dance Team", "beginner", "Ballroom dance", "ballroom@psu.edu", "Learn how to ballroom dance", "@PSUballroom", "fitness", add4);
-//        organizations.add(org1);
-//        organizations.add(org2);
-//        organizations.add(org3);
-//        organizations.add(org4);
-//        for (Organization organization : organizations) {
-//            System.out.println(organization.getOrgDetails()); 
-//        }
-//    }
-//    
-//    public void testAddress(){
-//        //test address class
-//        Address add1 = new Address(23, "Belmont Drive", "Robbinsville", "NJ", "08691");
-//        if (add1 != null){
-//            System.out.println("Test address created successfully");
-//            System.out.println(add1.toString());
-//            add1.setCity("");
-//            System.out.println(add1.toString());
-//            add1.setState("PA");
-//            System.out.println(add1.toString());
-//            add1.setStreetName("Main Street");
-//            System.out.println(add1.toString());
-//            add1.setStreetNum(0);
-//            System.out.println(add1.toString());
-//            add1.setStreetNum(222);
-//            System.out.println(add1.toString());
-//            add1.setZip("16802");
-//            System.out.println(add1.toString());
-//            add1.setCity("State College");
-//            System.out.println(add1.toString());
-//        }
-//        else {
-//            System.out.println("Instrument is null");
-//        }
-//    }
-//    
-//    public void testDate(){
-//        //test date class
-//        EventDate date1 = new EventDate(14, "Monday",  "1:00pm",9, 2020);
-//        System.out.println(date1.toString());
-//        date1.setDayOfWeek("Thursday");
-//        System.out.println(date1.toString());
-//        date1.setDateNum(15);
-//        System.out.println(date1.toString());
-//        date1.setDateNum(0);
-//        System.out.println(date1.toString());
-//        date1.setMonth(10);
-//        System.out.println(date1.toString());
-//        date1.setTime("11:30 am");
-//        System.out.println(date1.toString());
-//    }
-    
-//}
+  
+ 
