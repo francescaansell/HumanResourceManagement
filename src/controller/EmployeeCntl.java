461f9e6bed66034839de3e39df3b26d0f6cd8a64
Vincent Semrau <vss5123@psu.edu>
2022-03-02 22:04:55 -0500

    employee stuff all working for now git pull origin master!

--- src/controller/EmployeeCntl.java
+++ src/controller/EmployeeCntl.java
@@ -19,12 +19,87 @@ public class EmployeeCntl {
         this.employee = employee;
         this.view = view; 
     }
+
+    public void setFirstName(String firstName){
+        employee.setFirstName(firstName);
+    }
+
+    public void setLastName(String lastName){
+        employee.setLastName(lastName);
+    }
+
+    public void setUserName(String userName){
+        employee.setUserName(userName);
+    }
+
+    public void setEmployeeID(int employeeID){
+        employee.setEmployeeID(employeeID);
+    }
+
+    public void setPw(String pw){
+        employee.setPw(pw);
+    }
+
+    public void setDepartment(String department){
+        employee.setDepartment(department);
+    }
+
+    public void setNumLeaveDays(int numLeaveDays){
+        employee.setNumLeaveDays(numLeaveDays);
+    }
+
+    public void setFullTime(boolean fullTime){
+        employee.setFullTime(fullTime);
+    }
+
+    public void setPreviousTimesheets(PreviousTimesheets previousTimesheets){
+        employee.setPreviousTimesheets(previousTimesheets);
+    }
+
+    public String getFirstName(){
+        return employee.getFirstName();
+    }
+
+    public String getLastName(){
+        return employee.getLastName();
+    }
+
+    public String getUserName(String userName){
+        return employee.getUserName();
+    }
   
+    public int getEmployeeID(){
+        return employee.getEmployeeID();
+    }
+
+    public String getPw(String pw){
+        return employee.getPw();
+    }
+
+    public String getDepartment(){
+        return employee.getDepartment();
+    }
+
+    public int getNumLeaveDays(){
+        return employee.getNumLeaveDays();
+    }
+
+    public boolean getFullTime(){
+        return employee.getFullTime();
+    }
+
+    public PreviousTimesheets getPreviousTimesheets(){
+        return employee.getPreviousTimesheets();
+    }
+
+
 
     public void updateView(){
-    
+        view.printEmployeeDetails(employee.getFirstName(), employee.getLastName(), employee.getUserName(), employee.getDepartment(), employee.getEmployeeID(), employee.getFullTime());
     }
    
+    //    public Employee(String firstName, String lastName, String userName, String pw, String department, int employeeID, int numLeaveDays, PreviousTimesheets previousTimesheets, Boolean fullTime) {
+
 
  }
 
